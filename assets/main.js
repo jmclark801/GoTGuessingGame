e50397d3c4469281a13dc2b6169f43c9a4a29530
NunyaBizness <jmclark801@yahoo.com>
2019-04-02 21:22:00 -0600

    format update

--- assets/main.js
+++ assets/main.js
@@ -6,20 +6,20 @@ var lettersGuessed = [];
 var displayedCharacter = [];
 var randomCharacter = "";
 var characterImagePath = ""
-var randomCharacterArray = []; 
+var randomCharacterArray = [];
 var audio = new Audio('assets/music/GameOfThronesTheme.mp3');
 
 
 // Make initial API call
 var request = new XMLHttpRequest()
 request.open('GET', 'https://api.got.show/api/characters/', true)
-request.onload = function() {
+request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
   if (request.status >= 200 && request.status < 400) {
     data.forEach(character => {
-   
-      if (character.pageRank > 200 && character.imageLink){
+
+      if (character.pageRank > 200 && character.imageLink) {
         characters.push({
           name: character.name,
           image: "https://api.got.show" + character.imageLink
@@ -52,47 +52,47 @@ function resetGame() {
 }
 
 // Format the chracter to display underscores and spaces. Spaces are given to the user so they don't need to use a guess on them.
-function formatDisplayedCharacter(randomCharacter){
+function formatDisplayedCharacter(randomCharacter) {
   randomCharacterArray = randomCharacter.split('');
-  for(i= 0; i <randomCharacterArray.length; i++){
+  for (i = 0; i < randomCharacterArray.length; i++) {
     if (randomCharacterArray[i] === " ") {
       displayedCharacter.push(" ")
     } else {
-    displayedCharacter.push("_")
+      displayedCharacter.push("_")
     }
   }
 }
 
 // Determine  if the user's guess matches any letters in the hidden word.
-function determineIfMatch(userGuess){
-  for(i=0; i < displayedCharacter.length; i++){
-    if(userGuess === randomCharacterArray[i] || userGuess.toUpperCase() === randomCharacterArray[i]){
+function determineIfMatch(userGuess) {
+  for (i = 0; i < displayedCharacter.length; i++) {
+    if (userGuess === randomCharacterArray[i] || userGuess.toUpperCase() === randomCharacterArray[i]) {
       displayedCharacter[i] = userGuess;
     }
   }
   if (randomCharacterArray.indexOf(userGuess) == -1 && numberOfGuesses > 0) {
-    numberOfGuesses = numberOfGuesses -1;
+    numberOfGuesses = numberOfGuesses - 1;
   }
 }
 
-function determineIfWinner(){
-  if (numberOfGuesses === 0){
+function determineIfWinner() {
+  if (numberOfGuesses === 0) {
     displayResults("Lose");
   } else if (displayedCharacter.join('').toUpperCase() === randomCharacterArray.join('').toUpperCase()) {
     displayResults("Win");
-  } 
+  }
 }
 
-function evaluateGuess(keyPressed){
+function evaluateGuess(keyPressed) {
   var userGuess = ""
   var eligibleGuesses = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-  if (keyPressed !=="Meta"){
+  if (keyPressed !== "Meta") {
     userGuess = keyPressed;
   }
-  if (eligibleGuesses.includes(userGuess)){
+  if (eligibleGuesses.includes(userGuess)) {
     // If the letter hasn't been guessed, add it to the lettersGuessed array.
-    if (!lettersGuessed.includes(userGuess)){
+    if (!lettersGuessed.includes(userGuess)) {
       lettersGuessed.push(userGuess);
     }
   }
@@ -105,18 +105,18 @@ function evaluateGuess(keyPressed){
 }
 
 // This function is needed to prevent the spacebar from restarting the game accidentally
-function preventSpacebarDefault(event){
-  if ( event.which === 32 ) {
+function preventSpacebarDefault(event) {
+  if (event.which === 32) {
     event.preventDefault();
   }
 }
 
 // Upate the modal depending on if the user won or lost.
-function displayResults(result){
+function displayResults(result) {
   $('#play-again-modal').modal('show');
-  if (result ==="Lose"){
+  if (result === "Lose") {
     document.getElementById('modal-title').innerText = "Sorry, you lost.  The correct Answer was..." + randomCharacter;
-  } else if (result==="Win"){
+  } else if (result === "Win") {
     document.getElementById('modal-title').innerText = "Congratulations, You Won!  You correctly guess " + randomCharacter;
     wins += 1;
     document.getElementById('wins').innerText = "Wins: " + wins;
@@ -124,18 +124,18 @@ function displayResults(result){
   $("#character-image").attr("src", characterImage);
   audio.play();
   setTimeout(function () {
-      audio.pause();
-    }, 3000);
+    audio.pause();
+  }, 3000);
   lettersGuessed = [];
 }
 
 // Main Game 
-function newGame () {
- resetGame();
- formatDisplayedCharacter(randomCharacter);
- document.onkeyup = function(event){
-  var keyPressed = event.key;
-  preventSpacebarDefault(event);
-  evaluateGuess(keyPressed);
- };
+function newGame() {
+  resetGame();
+  formatDisplayedCharacter(randomCharacter);
+  document.onkeyup = function (event) {
+    var keyPressed = event.key;
+    preventSpacebarDefault(event);
+    evaluateGuess(keyPressed);
+  };
 }
